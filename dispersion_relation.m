2362869f876b5ffb5836a57870b13e2cc6c3c0d4
Nicolo' Fabbiane <nicolo@mech.kth.se>
2016-11-08 18:05:20 +0100

    dispersion relation updated using KS_init

--- dispersion_relation.m
+++ dispersion_relation.m
@@ -1,53 +1,50 @@
-clc, close all, clear all
+clc, clear all, %close all
+addpath('./lib')
 
 %% Parameters
 
 % Kuramoto
-alphamax = 1.68e-1;
-omegarmax = 2.67e-3;
-betamax = 2.15e-1;
+omegarmax = 2.5e-3; % maximum growth rate
+alphamax = 1.5e-1;  % alpha of maximum growth rate
+betamax = 1.5e-1;   % maximum unstable beta
+
+V  = 1.0;           % phase speed (= group speed, KS is not dispersive)
 
 P = 2 * alphamax^2;
 R = P^2/(4*omegarmax);
-S  = omegarmax*R/betamax^4;
-V  = 0.4;
+S = omegarmax*R/betamax^4;
 
 % space discretisation
-nqx =   96;
-nqz =   16;
-Lx  = 1000;
-Lz  =  150;
-Lf  =  200;
+NX =  72;           % number of modes in x
+NZ =  12;           % number of modes in z
+LX = 500;           % domain length (x)
+LZ = 200;           % domain width (z)
+Lf = 150;           % fringe length (x)
 
 
-%% Grid
-alpha0 = 2*pi / Lx;
-ialpha = -floor(nqx/2):ceil(nqx/2-1)';
-alpha  = alpha0 * ialpha;
-
-beta0 = 2*pi / Lz;
-ibeta = -floor(nqz/2):ceil(nqz/2-1)';
-beta  = beta0 * ibeta;
-
-[beta,alpha] = meshgrid(beta,alpha);
 
 
-%% Dispersion relation
-omegar = (P*(alpha.^2) - (alpha.^4 + S*beta.^4))/R;
-omegai = V*alpha.*(1 + beta.^2/(8*P));
+%% Initialization
+[~,~,~,omega,alpha,beta] = ks_init(P,R,S,V,LX,LZ,Lf,NX,NZ);
+
+% - reorder for better contours
+alpha = fftshift(alpha);
+beta  = fftshift(beta );
+omega = fftshift(omega);
+
+
 
 
 %% Plot
-ax = [[-1 1]*1.5*betamax [0 1]*2*alphamax];
+lvlr = (-1:.1:1) * omegarmax;
+lvli = (-1:.1:1) * V * 2*alphamax;
+
 figure(1); clf;
 
-subplot(2,1,1); contourf(beta(1,:),alpha(:,1),omegar,(-1:.01:1)*20*omegarmax,'LineColor','none'); hold on
-                contour(beta(1,:),alpha(:,1),omegar,[0 0],'-w','Linewidth',2); hold off
-                axis image; axis(ax); colorbar('EO');
-                cax = caxis; caxis([-1 1]*max(cax));
-                xlabel('\beta'); ylabel('\alpha'); title('\omega_r')
+subplot(2,1,1); contourf(alpha(:,1),beta(1,:),real(omega).',lvlr,'LineColor','none');
+                axis image; colorbar('EO'); colormap(gca,redblue);
+                xlabel('\alpha'); ylabel('\beta'); title('\omega_r')
 
-subplot(2,1,2); contourf(beta(1,:),alpha(:,1),omegai,(0:.1:1)*V*alphamax,'LineColor','none'); 
+subplot(2,1,2); contourf(alpha(:,1),beta(1,:),imag(omega).',lvli,'LineColor','none'); 
                 axis image; colorbar('EO');
-                cax = caxis; axis(ax); caxis([0 1]*max(abs(cax)));
-                xlabel('\beta'); ylabel('\alpha'); title('\omega_r')
+                xlabel('\alpha'); ylabel('\beta'); title('\omega_i')
