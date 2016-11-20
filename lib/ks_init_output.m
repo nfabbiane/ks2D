8f78757ddcd6c27020e94419d3fc83360df8b2eb
Nicolò Fabbiane <nfabbiane@gmail.com>
2016-11-20 12:07:56 +0100

    dCdxC added

--- lib/ks_init_output.m
+++ lib/ks_init_output.m
@@ -1,4 +1,4 @@
-function [C,Cfou,Cphy] = ks_init_output(pos,sigma,xx,zz,LX,LZ)
+function [C,Cfou,Cphy,dC,dCfou,dCphy] = ks_init_output(pos,sigma,xx,zz,LX,LZ)
 %
 %   [C,Cfou,Cphy] = KS_init_input(pos,sigma,xx,zz)
 %
@@ -10,17 +10,29 @@ nq  = nx*nz;
 % - compute input matrix in physical space
 Cphy = zeros(nx,nz,nout);
 Cfou = zeros(nx,nz,nout);
+dCphy = zeros(nx,nz,nout,2);
+dCfou = zeros(nx,nz,nout,2);
 
 for l = 1:nout
-    Cphy(:,:,l)  = exp(- ((xx-pos(l,1)).^2)/sigma(l,1).^2 ...
-                       - ((zz-pos(l,2)).^2)/sigma(l,2).^2 )/sqrt(prod(sigma(l,:)));
+    arg = - ((xx-pos(l,1)).^2)/sigma(l,1).^2 ...
+          - ((zz-pos(l,2)).^2)/sigma(l,2).^2 ;
+      
+    Cphy(:,:,l) = exp(arg)/sqrt(prod(sigma(l,:)));
                    
-    Cfou(:,:,l)  = conj(fft2(Cphy(:,:,l))) / nq;
+    dCphy(:,:,l,1)  = -1/sigma(l,2) * (-2 * (xx-pos(l,1))/sigma(l,1)) .* arg .* Cphy(:,:,l);
+    dCphy(:,:,l,2)  = -1/sigma(l,2) * (-2 * (zz-pos(l,2))/sigma(l,2)) .* arg .* Cphy(:,:,l);
+                   
+    Cfou(:,:,l)    = conj(fft2( Cphy(:,:,l)  )) / (nx*nz);
+    dCfou(:,:,l,1) = conj(fft2(dCphy(:,:,l,1))) / (nx*nz);
+    dCfou(:,:,l,2) = conj(fft2(dCphy(:,:,l,2))) / (nx*nz);
 end
 
 % - reorder for state space formulation
-C = zeros(nout,nq);
+C  = zeros(nout,nq);
+dC = zeros(nout,nq,2);
 
 for l = 1:nout
-    C(l,:) = conj(v2q(Cphy(:,:,l))) * LX*LZ;
+    C(l,:)    = conj(v2q( Cphy(:,:,l)  )) * LX*LZ;
+    dC(l,:,1) = conj(v2q(dCphy(:,:,l,1))) * LX*LZ;
+    dC(l,:,2) = conj(v2q(dCphy(:,:,l,2))) * LX*LZ;
 end
